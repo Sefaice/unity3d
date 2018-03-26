d49d8e8fb2abddfdbee6fba5fdcbe3b70e46614d
Sefaice <fanruichao1@live.com>
2018-03-26 19:41:57 +0800

    h

---
+++ hw1-Discrete Simulation Engine Basic/Tic-Tac-Toe/Assets/indexUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indexUI : MonoBehaviour {
+
+    public Texture yosimite;
+
+    void OnGUI()
+    {
+        
+        GUI.Button(new Rect((Screen.width-400)/2, (Screen.height - 400) / 2, 400, 400), yosimite);
+        //Debug.Log(Screen.width);
+        //Debug.Log( Screen.height);
+    }
+}
