f82439b9e7d43b6dd366354431e86b28e8315e43
Sefaice <fanruichao1@live.com>
2018-04-17 18:42:38 +0800

    ufo~

---
+++ hw4-Interaction/HitUFO/Assets/Scripts/DiskFactory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskFactory : MonoBehaviour {
+
+    private List<UFOManager> usedUFO;
+    private List<UFOManager> freeUFO;
+    private int unhiitedNum;
+
+    void Awake()
+    {
+        usedUFO = new List<UFOManager>();
+        freeUFO = new List<UFOManager>();
+        unhiitedNum = 0;
+    }
+
+    public UFOManager GetDisk(int round)
+    {
+        UFOManager neo;
+        if (freeUFO.Count != 0)
+        {
+            neo = freeUFO[0];
+            freeUFO.Remove(freeUFO[0]);
+            neo.Reborn(round);
+        }
+        else
+        {
+            neo = new UFOManager(round);
+        }
+        usedUFO.Add(neo);
+        return neo;
+    }
+
+    public void FreeDisk(UFOManager old)
+    {
+        usedUFO.Remove(old);//直接remove可以找到元素
+        freeUFO.Add(old);
+        //Debug.Log(usedUFO.Count);
+        //Debug.Log(freeUFO.Count);
+    }
+
+    void Update()
+    { 
+        foreach(UFOManager ufo in usedUFO)
+        {
+            if (ufo.GetPosition().z < -5)//z<-5 means player failed to hit it, so recycle it. 
+            {
+                FreeDisk(ufo);
+                unhiitedNum++;
+                break;
+            }
+        }
+    }
+
+    public int GetUnhittedNum()
+    {
+        int num = unhiitedNum;
+        unhiitedNum = 0;
+        return num;
+    }
+
+    public void GameStart()
+    {
+        while (usedUFO.Count != 0)
+        {
+            usedUFO[0].GetUFO().SetActive(false);
+            freeUFO.Add(usedUFO[0]);
+            usedUFO.Remove(usedUFO[0]);
+        }
+        unhiitedNum = 0;
+    }
+}
---
+++ hw4-Interaction/HitUFO/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+
+    IUserAction action;
+
+    private int life;//1 alive, 0 die, 2 win
+    string str = "Start";
+    private int score;
+    public int round;
+
+    // Use this for initialization
+    void Start()
+    {
+        action = Director.getInstance().currentSceneController as IUserAction;
+        score = 0;
+    }
+
+    void OnGUI()
+    {
+
+        if (GUI.Button(new Rect((Screen.width) / 2 - 350, Screen.height/2-90, 80, 50), str))
+        {
+            if (str == "Start")
+            {
+                action.GameStart();
+                str = "ReStart";
+            }
+            else
+            {
+                action.ReStart();
+            }
+        }
+        if (score >= 0)
+        {
+            string roundStr = "round " + round;
+            GUI.Button(new Rect((Screen.width ) / 2 - 350, Screen.height / 2, 80, 50), roundStr);
+        }
+        if (score < 0)
+        {
+            GUI.Button(new Rect((Screen.width) / 2-350, Screen.height/2, 80, 50), "Die!");
+        }
+        GUI.Button(new Rect((Screen.width) / 2-350 , Screen.height/2+90, 80, 50), score.ToString());
+    }
+    
+    public void UpdateScore(int _score)
+    {
+        score = _score;
+    }
+
+}
