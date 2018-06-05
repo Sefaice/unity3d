6a11e1ad65cfe5cccc5f1b0248e33e0b9d007dde
Sefaice <fanruichao1@live.com>
2018-06-05 22:58:44 +0800

    package ui

---
+++ hw8-ui/UI/Assets/Scripts/DragControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Camera camera;
+    public GameObject bag;
+    public GameObject storage;
+
+    private GameObject item;
+    private Vector3 oldPosition;
+    //private Vector3 offset;
+    private GameObject newItem;
+    private Vector3 newPosition;
+
+    public GraphicRaycaster m_CanvasUI;
+    public EventSystem eventSystem;
+
+
+    void Start()
+    {
+        item = null;
+        oldPosition = new Vector3();
+        newItem = null;
+        newPosition = new Vector3();
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        //Debug.Log("begindrag: " + data.pointerDrag);
+        item = data.pointerEnter;
+        oldPosition = item.transform.position;
+        //offset = item.transform.position - Input.mousePosition;
+        //拖动的物体处于最上层
+        //item.transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        //Debug.Log("dragging: " + data.pointerDrag);
+        if(item!=null && item.name!="Bag" && item.name!="Storage")
+        {
+            Vector3 pos = Input.mousePosition;
+            //自己试着转换一直不对，抄了博客中的转换方法，好像log出来鼠标的坐标没有加上ui相机的-250
+            Vector3 mmp = camera.ScreenToWorldPoint(pos + new Vector3(0, 0, 250));
+            item.transform.position = new Vector3(mmp.x, mmp.y, 0);
+            //防止拖动过程中旋转
+            item.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        //交换位置
+        //Debug.Log(data.pointerEnter);
+        if (false)
+        {
+            newItem = data.pointerEnter;
+            newPosition = newItem.transform.position;
+            newItem.transform.position = oldPosition;
+            item.transform.position = newPosition;
+        }
+        else//放回原位
+        {
+            if (item.transform.parent.gameObject.name == "Bag")
+            {
+                item.transform.position = oldPosition;
+                item.transform.eulerAngles = bag.transform.eulerAngles;
+            }
+            else
+            {
+                item.transform.position = oldPosition;
+                item.transform.eulerAngles = storage.transform.eulerAngles;
+            }
+        }
+        item = null;
+        oldPosition = new Vector3();
+        newItem = null;
+        newPosition = new Vector3();
+    }
+}
---
+++ hw8-ui/UI/Assets/Scripts/StorageWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorageWindow : MonoBehaviour
+{
+
+    //实现ui界面随着鼠标移动跟随方向旋转的效果
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        float halfWidth = Screen.width * 0.5f;
+        float halfHeight = Screen.height * 0.5f;
+        //clamp,小于最小值返回min，大于最大值返回max，否则返回原值
+        //Debug.Log(pos.y);
+        float x = Mathf.Clamp((pos.x - Screen.width / 2 - 200) / (Screen.width + 200), -1f, 1f);
+        float y = Mathf.Clamp((pos.y - Screen.height / 2) / Screen.height, -1f, 1f);
+        /*
+        rot = Vector2.Lerp(rot, new Vector2(x, y), Time.deltaTime * 5f);
+        transform.localRotation = mStart * Quaternion.Euler(-rot.y * range.y, rot.x * range.x, 0f);
+        */
+        //鼠标x方向偏差确定沿y轴旋转角度，y方向决定沿x轴旋转
+        transform.eulerAngles = new Vector3(5 * y, -15 * x, 0);
+    }
+}
+
